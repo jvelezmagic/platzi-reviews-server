383cbe3090d1aeda223d9296a9529ace9a013e89
Jesús Vélez Santiago <jvelezmagic@gmail.com>
2023-03-07 12:36:03 -0600

    feat: :construction: Initial configuration for GraphQL Yoga server
    At this point the app won't work due to the absence of a root query

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { PrismaModule } from 'nestjs-prisma';
 
+import { YogaDriver, YogaDriverConfig } from '@graphql-yoga/nestjs';
+import { GraphQLModule } from '@nestjs/graphql';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlatziScraperModule } from './platzi-scraper/platzi-scraper.module';
@@ -18,6 +21,10 @@ import configuration from './config/configuration';
     ScheduleModule.forRoot(),
     PrismaModule.forRoot({
       isGlobal: true,
+    }),
+    GraphQLModule.forRoot<YogaDriverConfig>({
+      driver: YogaDriver,
+      autoSchemaFile: 'schema.gql',
     }),
     PlatziScraperModule,
   ],
