70046760db597c32d892a636ec0272f1005303e5
Jesús Vélez Santiago <jvelezmagic@gmail.com>
2023-03-07 12:29:50 -0600

    feat: :bricks: Migrate to Fastify and apply ValidationPipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const port = configService.get<number>('PORT', 3000);
+  await app.listen(port);
 }
+
 bootstrap();
